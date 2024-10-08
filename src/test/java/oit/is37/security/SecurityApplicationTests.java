ee0a2963f835b9d6cee22a98b294e3b1502d2f04
B22017 <e1b22017@oit.ac.jp>
2024-10-08 14:12:23 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is37/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is37.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
